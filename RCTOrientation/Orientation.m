96ba4484de75adf29e20226c750b26c465fb7c8a
foobarbazbatban <jonathan@jastanton.com>
2015-11-02 20:45:59 -0800

    back to the basics

--- RCTOrientation/Orientation.m
+++ RCTOrientation/Orientation.m
@@ -3,13 +3,43 @@
 //
 
 #import "Orientation.h"
+#import "AppDelegate.h"
+
 
 @implementation AppDelegate (Orientation)
+
+- (NSUInteger)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
+  int orientation = [Orientation getOrientation];
+  switch (orientation) {
+    case 1:
+      return UIInterfaceOrientationMaskPortrait;
+      break;
+    case 2:
+      return UIInterfaceOrientationMaskLandscape;
+      break;
+    case 3:
+      return UIInterfaceOrientationMaskAllButUpsideDown;
+      break;
+    default:
+      return UIInterfaceOrientationMaskPortrait;
+      break;
+  }
+}
 
 @end
 
 
 @implementation Orientation
+
+@synthesize bridge = _bridge;
+
+static int _orientation = 3;
++ (void)setOrientation: (int)orientation {
+  _orientation = orientation;
+}
++ (int)getOrientation {
+  return _orientation;
+}
 
 - (instancetype)init
 {
@@ -31,7 +61,7 @@
   UIDeviceOrientation orientation = [[UIDevice currentDevice] orientation];
   NSString *orientationStr = [self getOrientationStr:orientation];
 
-  [self.bridge.eventDispatcher sendDeviceEventWithName:@"orientationDidChange"
+  [_bridge.eventDispatcher sendDeviceEventWithName:@"orientationDidChange"
                                               body:@{@"orientation": orientationStr}];
 }
 
@@ -54,18 +84,41 @@
   return orientationStr;
 }
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(getOrientation:(RCTResponseSenderBlock)callback)
+{
+  UIDeviceOrientation orientation = [[UIDevice currentDevice] orientation];
+  NSString *orientationStr = [self getOrientationStr:orientation];
+  callback(@[[NSNull null], orientationStr]);
+}
 
 RCT_EXPORT_METHOD(lockToPortrait)
 {
   NSLog(@"Locked to Portrait");
-  [[UIApplication sharedApplication] setStatusBarOrientation:UIInterfaceOrientationPortrait animated:YES];
+  [Orientation setOrientation:1];
+//  AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+//  delegate.orientation = 1;
+
 }
 
 RCT_EXPORT_METHOD(lockToLandscape)
 {
   NSLog(@"Locked to Landscape");
-  [[UIApplication sharedApplication] setStatusBarOrientation:UIDeviceOrientationLandscapeLeft animated:YES];
+  [Orientation setOrientation:2];
+//  AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+//  delegate.orientation = 2;
+
 }
+
+RCT_EXPORT_METHOD(unlockAllOrientations)
+{
+  NSLog(@"Unlock All Orientations");
+  [Orientation setOrientation:3];
+//  AppDelegate *delegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+//  delegate.orientation = 3;
+
+}
+
 
 @end
